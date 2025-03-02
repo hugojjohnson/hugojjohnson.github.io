9a833a1bfeb9ed4361e26ffac3d7bf49f618a1e8
Hugo Johnson <hugojohnson271@gmail.com>
2025-03-02 23:39:47 +1100

    one more try

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
     return (<><div className="w-full h-[50vh]">
         <div className="max-w-screen-xl h-screen mx-auto flex flex-col justify-center items-center">
             <div className=" flex flex-col md:flex-row items-center">
-                <h1 className="text-5xl sm:text-8xl drop-shadow-lg">Hi! I'm Hugo.. 👋</h1>
+                <h1 className="text-5xl sm:text-8xl drop-shadow-lg">Hi! I'm Hugo 👋</h1>
             </div>
             <p className="mt-10 text-xl text-center px-4 pb-48">I'm a curious, motivated student studying Software Engineering at the University of Sydney.</p>
         </div>
